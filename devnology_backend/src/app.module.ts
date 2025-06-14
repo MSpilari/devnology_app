a929913a68d76debe4490a219764aec0a8806b64
MSpilari <mb.spilari@hotmail.com>
2025-06-14 13:22:47 -0300

    feat(module): configure AppModule with ProductsModule and global CacheModule

--- devnology_backend/src/app.module.ts
+++ devnology_backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, CacheModule.register({ isGlobal: true, ttl: 300 })],
   controllers: [AppController],
   providers: [AppService],
 })
