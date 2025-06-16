5cef1711c65f0d402916e5eaa121ac115263b2c7
MSpilari <mb.spilari@hotmail.com>
2025-06-16 13:08:22 -0300

    feat: add OrdersModule and TypeOrmModules configuration

--- devnology_backend/src/app.module.ts
+++ devnology_backend/src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     ProductsModule,
+    OrdersModule,
     CacheModule.register({ isGlobal: true, ttl: 300000 }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: __dirname + '/database/db.sqlite',
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
