722a93ceb0fd2733b4a4822cae22fd90a1b6ecc5
MSpilari <mb.spilari@hotmail.com>
2025-06-18 12:38:53 -0300

    feat: add remote patterns to next config

--- devnology_frontend/next.config.ts
+++ devnology_frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "placeimg.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+    unoptimized: process.env.NODE_ENV === "development",
+  },
 };
 
 export default nextConfig;
